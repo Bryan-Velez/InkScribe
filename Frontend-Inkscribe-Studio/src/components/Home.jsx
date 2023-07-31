85aa0ab38b23c4063d8f4a443bab6a3e371eb7f2
Bryan <b.velez205@gmail.com>
2023-07-31 18:58:08 -0400

    css updates

--- Frontend-Inkscribe-Studio/src/components/Home.jsx
+++ Frontend-Inkscribe-Studio/src/components/Home.jsx
@@ -1,38 +1,35 @@
 import React from "react";
-import { Button, Image, Row, Col } from "react-bootstrap";
+import { Button, Image, Row, Col, Container } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <div className="home">
+    <Container className="home">
       <h1>Welcome to Inkscribe Studio</h1>
       <Row>
-        <Col>
-          <Image src=""></Image>
+        <Col xs={6} md={4}>
+          <Image src="https://github.com/Bryan-Velez/InkScribe/blob/main/Frontend-Inkscribe-Studio/src/assets/OK.png?raw=true" roundedCircle />
         </Col>
-        <Col>
+        <Col xs={6} md={4} style={{ fontSize:'20px'}}>
           <p>
             Unleash your imagination and embark on a thrilling journey into the
             world of comic book creation!
-          </p>
-          <p>
+        <br />
             Here, you have the power to craft captivating stories, bring
             characters to life, and design stunning visual narratives that will
             mesmerize your readers.
-          </p>
-          <p>
+         <br />
             Whether you're a seasoned artist or a budding creator, our creative
             space is the perfect canvas for your ideas. Dive into the world of
             superheroes, explore fantastical realms, or delve into
             thought-provoking tales.
-          </p>
-          <p>
+          <br />
             So, what are you waiting for? Join our vibrant community of creators
             and start weaving your comic book magic today!
           </p>
         </Col>
       </Row>
       {/* Render login or sign-up forms */}
-    </div>
+    </Container>
   );
 };
 
--- Frontend-Inkscribe-Studio/src/components/Nav.jsx
+++ Frontend-Inkscribe-Studio/src/components/Nav.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+
+  const navStyle = {
+   
+      marginRight: '10px', 
+      padding: '8px 16px',
+      textDecoration: 'none',
+      color: '#604aeb',
+      fontWeight: '900',
+      transition: 'color 0.25s',
+      backgroundColor: '#ff8800c4',
+      border: '1px solid #604aeb',
+      borderRadius: '50px',
+
+      
+    // .nav-link:hover {
+    //   color: #ff9564;
+    
+  }
   return (
     <div className="header">
       <img
@@ -11,8 +29,8 @@ const Nav = () => {
       />
       
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/comicbooks">Comic Books</Link>
+        <Link to="/" style={navStyle}>Home</Link>
+        <Link to="/comicbooks" style={navStyle}>Comic Books</Link>
       </nav>
     </div>
   );
